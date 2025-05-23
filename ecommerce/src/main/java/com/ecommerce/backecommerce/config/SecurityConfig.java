174baa77166b473842bad5322cbce86620e7386e
FrancoUtn <francosardiutn@gmail.com>
2025-05-23 18:56:50 -0300

    Integración de JWT + permisos para productos públicos

--- ecommerce/src/main/java/com/ecommerce/backecommerce/config/SecurityConfig.java
+++ ecommerce/src/main/java/com/ecommerce/backecommerce/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.ecommerce.backecommerce.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -30,6 +31,7 @@ public class SecurityConfig {
         return http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/productos/**").permitAll() // permite ver productos sin autenticació
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
